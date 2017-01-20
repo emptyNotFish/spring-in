a1dc22085742da9642744e147cfb74adaf8e486b
liolay <liulei@raiyee.com>
2017-01-20 16:07:28 +0800

    every request need Authentication

--- src/main/java/cn/ocoop/shiro/filter/HttpBasicAuthenticationFilter.java
+++ src/main/java/cn/ocoop/shiro/filter/HttpBasicAuthenticationFilter.java
@@ -3,10 +3,17 @@ package cn.ocoop.shiro.filter;
 import cn.ocoop.shiro.HttpBasicToken;
 import org.apache.shiro.authc.AuthenticationToken;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 /**
  * Created by liolay on 15-8-20.
  */
 public class HttpBasicAuthenticationFilter extends org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter {
+
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        return false;
+    }
 
     protected AuthenticationToken createToken(String username, String password,
                                               boolean rememberMe, String host) {
